447a16e2d83a4d4fc5103de5a0fa52a29ff168bb
Nikita <nv9779@gmail.com>
2024-07-23 18:38:53 +0300

    Добавить адаптив для блоков на главной

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core'
 import { HomeRoutingModule } from './home-routing.module'
 import { HomeComponent } from './home.component'
 import { CommonModule } from '@angular/common'
+import { TuiIslandModule } from '@taiga-ui/kit'
 
 @NgModule({
   declarations: [
     HomeComponent,
   ],
   imports: [
+    TuiIslandModule,
     HomeRoutingModule,
     CommonModule,
   ],
